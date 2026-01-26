fb598f6c00afb9693b0bb8ab117beb28f39454e5
DucHao-62133001 <hao.nd.62cnot@ntu.edu.vn>
2026-01-26 22:49:53 +0700

    Test Bai 5-6

---
+++ Tu_Hoc/Bai5/Bai5_HinhVe.java
@@ -0,0 +1,10 @@
+package Tu_Hoc.Bai5;
+
+public class Bai5_HinhVe {
+    public static void main(String[] args) {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+    }
+}
